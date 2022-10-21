6023677c3a890b161eaa0d55f3f9ce55ca05cc94
it21004322 <it21004322@my.sliit.lk>
2022-10-21 21:32:58 +0530

    initial commit

---
+++ app/src/main/java/com/example/myapp2/MainActivity2.java
@@ -0,0 +1,16 @@
+package com.example.myapp2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.bmi_calculator);
+
+    }
+}
