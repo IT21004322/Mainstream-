a968bebde6ce2526f53784405a8ca752f18f0057
IT21006166 <it21006166@my.sliit.lk>
2022-10-23 18:11:49 +0530

    commmit diary files

---
+++ app/src/main/java/com/example/myapp2/DiaryMainActivity.java
@@ -0,0 +1,40 @@
+package com.example.myapp2;
+//
+import androidx.appcompat.app.AppCompatActivity;
+//
+//import android.content.Intent;
+//import android.os.Bundle;
+//import android.view.View;
+//import android.widget.Button;
+//
+public class DiaryMainActivity extends AppCompatActivity {
+//
+//    Button b1, b2;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.diary_activity_main);
+//
+//        b1 = findViewById(R.id.btn_dailyView);
+//        b2 = findViewById(R.id.btn_fitness);
+//        b1.setOnClickListener(new View.OnClickListener() {
+//                                  @Override
+//                                  public void onClick(View v) {
+//                                      Intent i = new Intent(DiaryMainActivity.this, DiaryDailyview.class);
+//                                      startActivity(i);
+//                                  }
+//                              }
+//        );
+//
+//        b2.setOnClickListener(
+//                new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        Intent i = new Intent(DiaryMainActivity.this, DiaryLogs.class);
+//                        startActivity(i);
+//                    }
+//                }
+//        );
+//    }
+}
