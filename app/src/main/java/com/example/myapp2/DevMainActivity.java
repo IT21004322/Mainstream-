bc3df9c07aa5cffe1f4d01dbdc3bc20e888a8737
Devindu Samarasinghe <it21003332@my.sliit.lk>
2022-10-30 23:41:30 +0530

    Fluid Implementation

--- app/src/main/java/com/example/myapp2/DevMainActivity.java
+++ app/src/main/java/com/example/myapp2/DevMainActivity.java
@@ -7,6 +7,11 @@ import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import com.example.myapp2.fluid.DevFluidChart;
+import com.example.myapp2.fluid.DevFluidInput;
+import com.example.myapp2.sleep.DevSleepChart;
+import com.example.myapp2.sleep.DevSleepInput;
+
 public class DevMainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
@@ -14,24 +19,40 @@ public class DevMainActivity extends AppCompatActivity implements View.OnClickLi
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dave_activity_main);
 
-        CardView card2 = findViewById(R.id.weightChart);
-        CardView card1 =  findViewById(R.id.fluid);
+        CardView sleepMain = findViewById(R.id.sleep);
+        CardView sleepChart = findViewById(R.id.sleepChart);
+        CardView fluid =  findViewById(R.id.fluid);
+        CardView fluidChart = findViewById(R.id.fluidIntake);
 
-        card1.setOnClickListener(new View.OnClickListener() {
+        sleepMain.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(DevMainActivity.this, com.example.sculptr.DevFluidChart.class);
+                Intent i = new Intent(DevMainActivity.this, DevSleepInput.class);
                 startActivity(i);
             }
         });
-        card2.setOnClickListener(new View.OnClickListener() {
+        fluid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(DevMainActivity.this, DevFluidChart.class);
+                startActivity(i);
+            }
+        });
+        fluidChart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(DevMainActivity.this, DevFluidInput.class);
+                startActivity(i);
+            }
+        });
+
+        sleepChart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(DevMainActivity.this, DevSleepChart.class);
                 startActivity(i);
             }
         });
-
     }
 
     @Override
