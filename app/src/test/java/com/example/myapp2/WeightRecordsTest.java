c036056a993a8b39c4c1e08b8e132aaa98f8f436
IT21006166 <it21006166@my.sliit.lk>
2022-10-30 23:59:39 +0530

    adding forms and activities

---
+++ app/src/test/java/com/example/myapp2/WeightRecordsTest.java
@@ -0,0 +1,24 @@
+package com.example.myapp2;
+
+import static org.junit.Assert.assertEquals;
+
+import com.example.sculptr.models.WeightRecords;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class WeightRecordsTest {
+    private WeightRecords weightRecords;
+
+    @Before
+    public void setUp(){
+        weightRecords = new WeightRecords();
+    }
+
+    @Test
+    public void testValidateWeight(){
+        boolean negativeResult = weightRecords.validateWeight("-10");
+        assertEquals(false,negativeResult);
+    }
+
+}
